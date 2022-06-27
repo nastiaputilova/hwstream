64e8245910190f0726c0dc37a9d177ea9fac6e70
anastasiaputilova <anastasiaputilova@gmail.com>
2022-06-27 22:12:53 +0100

    Initial commit

---
+++ main/java/com/example/hwstream/HwstreamApplication.java
@@ -0,0 +1,13 @@
+package com.example.hwstream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwstreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HwstreamApplication.class, args);
+    }
+
+}
